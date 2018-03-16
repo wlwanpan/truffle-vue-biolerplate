6c5294cf7aa326fb294f52c37628fe939f3184a1
Estelle Wan <eccheung@amazon.com>
2018-03-15 20:54:25 -0700

    added dashboard boilerplate

--- src/main.js
+++ src/main.js
@@ -1,34 +1,25 @@
 import Vue from 'vue'
-import App from './App'
-import Web3 from 'web3'
-import router from './router'
-import store from './store/store'
-import VModal from 'vue-js-modal'
-import mixins from './js/mixins'
+import VueRouter from 'vue-router'
+import App from './App.vue'
 
-Vue.config.productionTip = false
-Vue.use(VModal, { dynamic: true })
+// LightBootstrap plugin
+import LightBootstrap from './light-bootstrap-main'
 
-window.addEventListener('load', function () {
-  if (typeof web3 !== 'undefined') {
-    console.log('Web3 injected browser: OK.')
-    window.web3 = new Web3(window.web3.currentProvider)
-  } else {
-    console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
-    // Fallback to local node
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-  }
+// router setup
+import routes from './routes/routes'
+// plugin setup
+Vue.use(VueRouter)
+Vue.use(LightBootstrap)
 
-  // Import Mixins Methods and Inject in All Components
-  Vue.mixin(mixins)
-
-  /* eslint-disable no-new */
-  new Vue({
-    el: '#app',
-    router,
-    store,
-    template: '<App/>',
-    components: { App }
-  })
+// configure router
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkActiveClass: 'nav-item active'
 })
 
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
